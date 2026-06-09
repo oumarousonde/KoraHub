c8b34905d5197f9d9cb31bd37729297d006fa612
oumarousonde <oumarousonde240@gmail.com>
2026-06-09 01:37:38 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='korahub-v4';
-const FILES=['/','/index.html','/korahub-recherche.html','/korahub-auth.html','/korahub-kids.html','/korahub-templates.html','/korahub-confirmation.html','/korahub-comment-vendre.html'];
+const CACHE='korahub-v7';
+const FILES=['/','/index.html','/korahub-auth.html','/korahub-recherche.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/index.html'))));});
